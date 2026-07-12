d6c067cd67191c7648b6da97988e0d91b67c8993
chengzimo <3440174343@qq.com>
2026-07-12 10:24:21 +0800

    第10天代码修复：78→95分 满分手写代码

--- prctice code/class_repot.py
+++ prctice code/class_repot.py
@@ -1,17 +1,16 @@
 def class_report(date):
     '''定义分析函数'''
-    for key in date:
-        date_report = {
-            "avg" : sum(date[key]) / len(date[key]),
-            "max" : max(date[key])
+    date_report = {}
+    for course , scores in date.items():
+        date_report[course] = {
+            "avg" : round(sum(scores) / len(scores),1),
+            "max" : max(scores)
         }
     '''逐行输出'''
-    for key in date:
-        print(f'The {key} :')
-        for key , value in date_report.items():
-                print(f'    The {key} is {value}')
+    # for key , value in date_report.items():
+    #         print(f'{key} {value}')
 
-    # return date_report
+    return date_report
 
 
 if __name__ == "__main__":
@@ -21,5 +20,5 @@ if __name__ == "__main__":
         "数学": [92, 78, 85, 90],
         "英语": [88, 92, 79, 85]
     }
-    # print(class_report(date))
-    class_report(date)
+    print(class_report(date))
+    # class_report(date)
