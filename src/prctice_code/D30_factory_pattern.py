98930bf544f49d4d38e72731fd95d8fb884e4b82
chengzimo <3440174343@qq.com>
2026-08-02 21:58:06 +0800

    第30天：设计模式 — 99分 🏆

---
+++ src/prctice_code/D30_factory_pattern.py
@@ -0,0 +1,50 @@
+'''
+用工厂模式实现一个通知系统：
+
+EmailNotifier, SMSNotifier, PushNotifier 三个类，都有 send(msg) 方法
+每个 send 打印不同的内容
+NotifierFactory.create(type) 根据类型返回对应通知器
+未知类型抛 ValueError
+'''
+
+class EmailNotifier:
+
+    def send(self , msg):
+        print(f'email:{msg}')
+
+class SMSNotifier:
+
+    def send(self , msg):
+        print(f'SMS: {msg}')
+
+class PushNotifier:
+
+    def send(self , msg):
+        print(f'Push :{msg}')
+
+class NotifierFactory:
+    _PushType = {
+        "email": EmailNotifier,
+        "sms": SMSNotifier,
+        "push": PushNotifier
+    }
+
+    @classmethod
+    def create(cls , notify_type):
+        push_type = cls._PushType.get(notify_type)
+        if push_type is None:
+            raise ValueError(f'未知通知类型:{notify_type}')
+        return push_type()
+
+
+#测试
+n1 = NotifierFactory.create('email')
+n1.send('steam:你的交易....')
+
+n2 = NotifierFactory.create('sms')
+n2.send('你的steam修改密码验证码:114514')
+
+n3 = NotifierFactory.create('push')
+n3.send('你有一条新的steam消息')
+
+NotifierFactory.create('wechat')
