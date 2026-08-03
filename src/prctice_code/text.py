222c858f0ee195bb3bb3538607242cee4d98325c
chengzimo <3440174343@qq.com>
2026-08-03 15:43:38 +0800

    新学一个魔法方法__call__

--- src/prctice_code/text.py
+++ src/prctice_code/text.py
@@ -9,9 +9,33 @@
 # print(passwords)
 
 # 问题1：找2个bug
-from collections import defaultdict
-d = defaultdict(list)
-pairs = [("a", 1), ("a", 3), ("b", 2)]
-for k, v in pairs:
-    d[k] += v          # bug在这里
-print(dict(d))
+# from collections import defaultdict
+# d = defaultdict(list)
+# pairs = [("a", 1), ("a", 3), ("b", 2)]
+# for k, v in pairs:
+#     d[k] += v          # bug在这里
+# print(dict(d))
+
+
+
+class add:
+    def __init__(self , count):
+        self.count = count
+
+    def __str__(self):
+        return f'{self.count}'
+
+    def __add__(self, other):
+        new_count = self.count + other
+        return add(new_count)
+
+    #让类能像函数一样调用
+    def __call__(self, other):
+        new_count = self.count + other
+        return add(new_count)
+
+addTwo = add(2)
+print(addTwo)
+print(addTwo + 5)
+print(addTwo(3))
+print(addTwo(3)(5))
